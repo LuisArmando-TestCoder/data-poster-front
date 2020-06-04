64ba13a3556521bfda7525ea6e7ef0f92eb50986
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-06-04 14:36:56 -0600

    add data-poster

---
+++ src/components/pages/home/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <div>
+      <input type="search" v-model="hash">
+      <a :href="url+'/'+hash">Search data</a>
+    </div>
+    <form :action="url" 
+          method="post"
+          @submit="getPostedDataHash">
+      <button type="submit">Submit data</button>
+      <textarea v-model="data" id="data-poster" cols="30" rows="10"></textarea>
+    </form>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data: () => ({
+    hash: '',
+    url: 'https://data-poster.herokuapp.com/data',
+    data: ''
+  }),
+  props: {
+    msg: String
+  },
+  methods: {
+    getPostedDataHash(e) {
+      e.preventDefault();
+      fetch(this.url, {
+        method: 'POST',
+        body: JSON.stringify({
+          data: this.data
+        })
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
