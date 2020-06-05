7880abf05949be1193353250a01811c587f1fb59
myl-consultorias <luisarmando.murillobaltodano@gmail.com>
2020-06-04 23:35:14 -0600

    add front v1

--- src/components/pages/home/HomePage.vue
+++ src/components/pages/home/HomePage.vue
@@ -1,25 +1,29 @@
 <template>
-  <section>
+  <section class="wrapper">
     <div>
-      <input type="search" v-model="hash">
-      <a :href="url+'/'+hash">Search data</a>
+      <input type="number" v-model="hash" class="search-hash-input neon-text" placeholder="only valid hashes">
+      <a :href="url+'/'+hash" target="_blank" class="search-hash-anchor cyber-text neon-text">Open data</a>
     </div>
     <form :action="url" 
           method="post"
           @submit="getPostedDataHash">
-      <button type="submit">Submit data</button>
-      <textarea v-model="data" id="data-poster" cols="30" rows="10"></textarea>
+      <p v-if="postedDataHash || isLoading" :class="isLoading">{{postedDataHash}}</p>
+      <textarea v-model="data" id="data-poster" class="submit-data submit-data-textarea neon-text"></textarea>
+      <button type="submit" class="cyber-text neon-text submit-data">Submit data</button>
     </form>
   </section>
 </template>
 
 <script>
+
 export default {
   name: 'HomePage',
   data: () => ({
     hash: '',
     url: 'https://data-poster.herokuapp.com/data',
-    data: ''
+    data: '',
+    postedDataHash: '',
+    isLoading: ''
   }),
   props: {
     msg: String
@@ -27,16 +31,97 @@ export default {
   methods: {
     getPostedDataHash(e) {
       e.preventDefault();
+      this.isLoading = 'isLoading';
       fetch(this.url, {
         method: 'POST',
         body: JSON.stringify({
           data: this.data
         })
       })
+      .then(r => r.text())
+      .then(hash => {
+        this.postedDataHash = hash;
+        this.isLoading = '';
+      });
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+$neon-color-1: #0ff;
+html {
+  --blur: 3px;
+  --neon-color: #{$neon-color-1};
+  --neon-light: 0 0 var(--blur) var(--neon-color);
+  --cyber-font: monospace;
+}
+.submit-data-textarea {
+  height: 290px;
+  width: 290px;
+}
+.submit-data {
+  background: transparent;
+  padding: 10px;
+}
+.cyber-text {
+  font-family: var(--cyber-font);
+  letter-spacing: 5px;
+  text-transform: uppercase;
+}
+.neon-text {
+  text-shadow: var(--neon-light);
+  color: var(--neon-color);
+}
+.search-hash-anchor {
+  text-align: end;
+}
+.search-hash-input {
+  width: 290px;
+  font-family: var(--cyber-font);
+  padding: 10px;
+  background: transparent;
+  border: 1px solid var(--neon-color);
+  box-shadow: var(--neon-light), inset var(--neon-light);
+  color: $neon-color-1 * 0.8;
+  transition: 0.5s;
+  &:focus {
+    --blur: 6px;
+    outline: none;
+  }
+}
+
+.wrapper {
+  background: #101010;
+  color: #f0f0f0;
+  font-family: var(--cyber-font);
+  display: grid;
+  min-height: 100vh;
+  & > * {
+    padding: 5vh;
+    display: grid;
+    place-content: center;
+    grid-gap: 25px;
+    & > * {
+      display: grid;
+      place-content: center;
+      grid-gap: 25px;
+    }
+  }
+}
+
+.isLoading::before {
+  content: '.';
+  display: block;
+  animation: loadingText 0.75s linear infinite;
+}
+
+@keyframes loadingText {
+  33% {
+    content: '..';
+  }
+  66% {
+    content: '...'; 
+  }
+}
 </style>
